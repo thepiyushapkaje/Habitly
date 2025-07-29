2cfb682691813701d7a3fbe659c4204abf90fd56
Piyush Apkaje <apkajepiyush@gmail.com>
2025-07-29 17:34:53 +0530

    Added date updation table.

--- app/src/main/java/com/nextbigthing/habitly/room/AppDatabase.kt
+++ app/src/main/java/com/nextbigthing/habitly/room/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.nextbigthing.habitly.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Habit::class], version = 1)
+@Database(entities = [Habit::class, SaveDate::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun habitDao(): HabitDao
 }
